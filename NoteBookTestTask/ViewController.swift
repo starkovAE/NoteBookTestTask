a072640a1ca611f97332b3b89893ed13932d7cb1
starkovAE <swix456@gmail.com>
2022-06-29 13:13:44 +0500

    Initial Commit

---
+++ NoteBookTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteBookTestTask
+//
+//  Created by Александр Старков on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
